f33ea7fee74b06a67d90863788040154d94e1c73
yongma16 <17685043634@163.com>
2020-11-03 22:15:02 +0800

    Spring学习日记

---
+++ src/main/java/com/yma16/springrecord/SpringrecordApplication.java
@@ -0,0 +1,13 @@
+package com.yma16.springrecord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringrecordApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringrecordApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yma16/springrecord/SpringrecordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yma16.springrecord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringrecordApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
