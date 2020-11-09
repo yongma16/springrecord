14b479a509ec9b6c2dc0bf9ef3ad2bef583aa4da
yongma16 <17685043634@163.com>
2020-11-09 17:08:28 +0800

    接口层的增、删、改、查方法

--- src/main/java/dao/BookDao.java
+++ src/main/java/dao/BookDao.java
@@ -6,4 +6,5 @@ public interface BookDao {
     public  void add(Book book);//添加book表数据
     public void updatebook(Book book);//修改表数据
     public void deletebook(Book book);//删除数据
+    public int selecCount();//查询数据库
 }
