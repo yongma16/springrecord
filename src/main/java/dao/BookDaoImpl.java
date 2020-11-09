ddee58e66e97f658d74a06f289a8b7d3cd46df58
yongma16 <17685043634@163.com>
2020-11-09 17:08:53 +0800

    类实现接口的增、删、改、查方法

--- src/main/java/dao/BookDaoImpl.java
+++ src/main/java/dao/BookDaoImpl.java
@@ -20,17 +20,24 @@ public class BookDaoImpl implements BookDao {
 
     @Override
     public void updatebook(Book book) {
-        String sql="update bookuser set username=? where id=?";//原生语句 修改数据
-        Object[] args={book.getUsername(),book.getId()};//只修改用户名
+        String sql="update bookuser set username=?,userpassword=? where id=?";//原生语句 修改数据
+        Object[] args={book.getUsername(),book.getUserpassword(),book.getId()};//只修改用户名使用get
         int update=jdbctemplate.update(sql,args);
         System.out.println(update);
     }
 
     @Override
     public void deletebook(Book book) {
-        String sql="delete from bookuser where id=?";//原生语句 更新数据
+        String sql="delete from bookuser where id=?";//原生语句 删除数据
         Object[] args={book.getId()};
         int update=jdbctemplate.update(sql,args);
         System.out.println(update);
+    }
+
+    @Override
+    public int selecCount() {
+        String sql="select count(*) from bookuser";
+        Integer count=jdbctemplate.queryForObject(sql,Integer.class);//第二个参数为返回的类型
+        return count;
     }
 }
