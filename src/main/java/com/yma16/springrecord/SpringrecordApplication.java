672bd2dfd021fba82ed0e026d70119be1cc52614
yongma16 <17685043634@163.com>
2020-11-09 17:10:07 +0800

    jdbc实现阿里数据库连接池的增删改查

--- src/main/java/com/yma16/springrecord/SpringrecordApplication.java
+++ src/main/java/com/yma16/springrecord/SpringrecordApplication.java
@@ -1,11 +1,13 @@
-package com.yma16.springrecord;//package com.yma16.springrecord;
+package com.yma16.springrecord;
 import bean_test.Outbean;
 import bean_test.Strange;
 import bean_test.User;
+import entity.Book;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import service.BookService;
 
 @SpringBootApplication
 public class SpringrecordApplication {
@@ -25,10 +27,40 @@ public class SpringrecordApplication {
         Strange s1=context2.getBean("s01",Strange.class);
         System.out.println(s1);
 
-        System.out.println("外部bean");
-        ApplicationContext context3= new ClassPathXmlApplicationContext("bean02.xml");
-        Outbean o1=context3.getBean("s01", Outbean.class);
-        System.out.println(o1);
+//        System.out.println("外部bean");
+//        ApplicationContext context3= new ClassPathXmlApplicationContext("bean02.xml");
+//        Outbean o1=context3.getBean("s01", Outbean.class);
+//        System.out.println(o1);
+
+        ApplicationContext context4=new ClassPathXmlApplicationContext("bean06.xml");
+        BookService jdbc=context4.getBean("bookService",BookService.class);//自动注解
+        System.out.println(jdbc);//已执行
+        Book user01=new Book();//添加的数据
+        user01.setUsername("yma16");
+        user01.setUserpassword("password");
+        user01.setId(666);//条件
+        //问题所在！
+        jdbc.addBook(user01);//实现接口
+        //插入数据
+        System.out.println("insert成功！");
+        //
+
+        Book user02=new Book();//修改的数据
+        user02.setUsername("修改的用户名");
+        user02.setUserpassword("修改的密码");
+        user02.setId(666);//条件
+        jdbc.updateBook(user02);//实现修改的接口
+        System.out.println("update成功！");
+
+
+//        Book user03=new Book();//删除的数据id
+//        user03.setId(666);//条件
+//        jdbc.deleteBook(user03);//删除的数据
+
+        //查询的数据
+        jdbc.selectCount();//实现查询的接口
+        System.out.println("查询成功！");
     }
 
 }
+
